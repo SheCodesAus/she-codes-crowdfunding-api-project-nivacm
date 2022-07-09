860fe1197f8b9c0bd101f0635199ca930bfcf3ad
nivacm <55969728+nivacm@users.noreply.github.com>
2022-07-10 01:17:33 +1000

    commit

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -8,10 +8,6 @@ from rest_framework import status, permissions
 from .permissions import IsOwnerOrReadOnly
 
 class ProjectList(APIView):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly
-        ]
-
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -66,6 +62,7 @@ class ProjectDetail(APIView):
 
 
 class PledgeList(APIView):
+    
     def get(self, request):
         pledges = Pledge.objects.all()
         serializer = PledgeSerializer(pledges, many=True)
